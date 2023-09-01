41d9d105c4a0e3328c108a507544ad972983d653
Wamique Abro <wamik.abro212@gmail.com>
2023-09-01 18:48:01 +0500

    Take BloodType and Gender back to Enums

--- Functionality/Enums/Gender.cs
+++ Functionality/Enums/Gender.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SchoolManagementSystem
+namespace SchoolManagementSystem.Functionality.Enums
 {
     public enum Gender
     {
