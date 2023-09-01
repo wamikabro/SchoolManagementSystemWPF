7ae97697666c541042e68d145b192fc29b0af5aa
Wamique Abro <wamik.abro212@gmail.com>
2023-09-01 18:42:30 +0500

    Put Gender in main namespace

--- Functionality/Enums/Gender.cs
+++ Functionality/Enums/Gender.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SchoolManagementSystem.Functionality.Enums
+namespace SchoolManagementSystem
 {
     public enum Gender
     {
