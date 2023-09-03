c5f746a77f7dadc96e59efb32f48228fdc7ffd56
Wamique Abro <wamik.abro212@gmail.com>
2023-09-03 20:22:16 +0500

    Store the user added data to the Database.

--- User Controls/AddStudentUC.xaml.cs
+++ User Controls/AddStudentUC.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 using System.Data.SqlClient;
 using SchoolManagementSystem.Functionality;
 using SchoolManagementSystem.Functionality.Enums;
+using System.Data;
 
 namespace SchoolManagementSystem.User_Controls
 {
@@ -30,9 +31,10 @@ namespace SchoolManagementSystem.User_Controls
             DOADatePicker.SelectedDate = DateTime.Today;
         }
 
-        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-RUINSQ2\\SQLEXPRESS;Initial Catalog=SchoolManagementSystem;Integrated Security=True");
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-RUINSQ2\\SQLEXPRESS;Initial Catalog=SchoolManagementSystem;Integrated Security=True");
+
         private void SubmitButton_Click(object sender, RoutedEventArgs e)
-        {
+        {       
             // Fetching the selections
             DateTime dateOfBirth;
             try
@@ -79,9 +81,43 @@ namespace SchoolManagementSystem.User_Controls
             String guardianName = GuardianNameTextBox.Text;
             try
             {
+                // Data verification will be done in the class
                 Student student = new Student(firstName, lastName, fatherName, gender, grade,
                     phoneNumber, email, bloodGroup, address, guardianName, dateOfBirth,
                     dateOfAdmissions);
+                try
+                {
+
+                    // Store data in the Database
+                    SqlCommand storeStudent = new
+                        SqlCommand("INSERT INTO StudentTable VALUES (@FirstName, @LastName, @FatherName, @Gender, @Grade, @PhoneNumber, @Email, @BloodGroup, @Address, @GuardianName, @DateOfBirth, @DateOfAdmissions)", con);
+                    // command type
+                    storeStudent.CommandType = CommandType.Text;
+
+                    // placement
+                    storeStudent.Parameters.AddWithValue("@FirstName", firstName);
+                    storeStudent.Parameters.AddWithValue("@LastName", lastName);
+                    storeStudent.Parameters.AddWithValue("@FatherName", fatherName);
+                    storeStudent.Parameters.AddWithValue("@Gender", gender);
+                    storeStudent.Parameters.AddWithValue("@Grade", grade);
+                    storeStudent.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+                    storeStudent.Parameters.AddWithValue("@Email", email);
+                    storeStudent.Parameters.AddWithValue("@BloodGroup", bloodGroup);
+                    storeStudent.Parameters.AddWithValue("@Address", address);
+                    storeStudent.Parameters.AddWithValue("@GuardianName", guardianName);
+                    storeStudent.Parameters.AddWithValue("@DateOfBirth", dateOfBirth);
+                    storeStudent.Parameters.AddWithValue("@DateOfAdmissions", dateOfAdmissions);
+
+
+
+                    con.Open();
+                    storeStudent.ExecuteNonQuery();
+                    con.Close();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
 
             }
             catch (ArgumentException ex)
