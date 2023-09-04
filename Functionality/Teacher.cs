0824272f349c8b1d33bd3787360023c034abbae5
Wamique Abro <wamik.abro212@gmail.com>
2023-09-04 20:51:59 +0500

    Make Function to Store Teacher Information in Database

--- Functionality/Teacher.cs
+++ Functionality/Teacher.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using SchoolManagementSystem.Functionality.Enums;
 
 namespace SchoolManagementSystem.Functionality
@@ -35,6 +38,42 @@ namespace SchoolManagementSystem.Functionality
                 }
 
             } 
+        }
+
+        public void StoreTeacher()
+        {
+            try
+            {
+                // Store data in the Database
+                SqlCommand storeTeacher = new
+                    SqlCommand("INSERT INTO TeacherTable VALUES (@FirstName, @LastName, @FatherName, @Gender, @Grade, @PhoneNumber, @Email, @BloodGroup, @Address, @Subject, @DateOfBirth, @DateOfAdmissions)", con);
+                // command type
+                storeTeacher.CommandType = CommandType.Text;
+
+                // placement
+                storeTeacher.Parameters.AddWithValue("@FirstName", firstName);
+                storeTeacher.Parameters.AddWithValue("@LastName", lastName);
+                storeTeacher.Parameters.AddWithValue("@FatherName", fatherName);
+                storeTeacher.Parameters.AddWithValue("@Gender", gender.ToString());
+                storeTeacher.Parameters.AddWithValue("@Grade", grade);
+                storeTeacher.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+                storeTeacher.Parameters.AddWithValue("@Email", email);
+                storeTeacher.Parameters.AddWithValue("@BloodGroup", bloodGroup.ToString());
+                storeTeacher.Parameters.AddWithValue("@Address", address);
+                storeTeacher.Parameters.AddWithValue("@Subject", subject);
+                storeTeacher.Parameters.AddWithValue("@DateOfBirth", dateOfBirth);
+                storeTeacher.Parameters.AddWithValue("@DateOfAdmissions", dateOfAdmissions);
+
+
+
+                con.Open();
+                storeTeacher.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
