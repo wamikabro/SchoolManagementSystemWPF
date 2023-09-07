d189d2bde8fc88a849dd2df8c3aaec23d7783e18
Wamique Abro <wamik.abro212@gmail.com>
2023-09-07 20:20:07 +0500

    Fetch Data From Database's TeacherTable and Show in DataGrid and keep it ReadOnly

--- User Controls/SearchTeacherUC.xaml.cs
+++ User Controls/SearchTeacherUC.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,24 @@ namespace SchoolManagementSystem.User_Controls
         public SearchTeacherUC()
         {
             InitializeComponent();
+            LoadTeacherDataGrid();
+        }
+
+        // database connection 
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-RUINSQ2\\SQLEXPRESS;Initial Catalog=SchoolManagementSystem;Integrated Security=True");
+
+        public void LoadTeacherDataGrid()
+        {
+            SqlCommand loadTeacherData = new
+                SqlCommand("SELECT * FROM TeacherTable", con);
+            DataTable dataTable = new DataTable();
+
+            con.Open();
+            SqlDataReader sqlDataReader = loadTeacherData.ExecuteReader();
+            dataTable.Load(sqlDataReader);
+            con.Close();
+
+            TeacherDataGrid.ItemsSource = dataTable.DefaultView;
         }
     }
 }
