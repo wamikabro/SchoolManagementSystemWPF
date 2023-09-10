b40667490e18865743bc5d26bb69ef0f72265d7d
Wamique Abro <wamik.abro212@gmail.com>
2023-09-10 17:26:23 +0500

    Connect Report Email sender to Default Mail

--- User Controls/ReportUC.xaml.cs
+++ User Controls/ReportUC.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,28 @@ namespace SchoolManagementSystem.User_Controls
         public ReportUC()
         {
             InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string subject = SubjectTextBox.Text;
+                string issue = IssueTextBox.Text;
+
+                // Specify the recipient email address (replace with the desired email address)
+                string recipientEmail = "wamik.abro212@gmail.com";
+
+                // Compose the mailto URL with subject, body, and recipient
+                string mailtoUrl = $"mailto:{recipientEmail}?subject={Uri.EscapeDataString(subject)}&body={Uri.EscapeDataString(issue)}";
+
+                // Start the default email client
+                Process.Start(mailtoUrl);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
         }
     }
 }
