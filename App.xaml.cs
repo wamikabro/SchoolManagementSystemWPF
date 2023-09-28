7ecd04d62c6593f9ea3a4314851fd13cdf73822e
Wamique Abro <wamik.abro212@gmail.com>
2023-09-28 13:03:08 +0500

    Set the last time saved theme in the startup of the application and remove the initial resources from App.xaml because now the logic is being handled by code behind pov: App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,5 +15,43 @@ namespace SchoolManagementSystem
     /// </summary>
     public partial class App : Application
     {
+        // SQL Connection
+        protected SqlConnection con;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            con = new SqlConnection("Data Source=DESKTOP-RUINSQ2\\SQLEXPRESS;Initial Catalog=SchoolManagementSystem;Integrated Security=True");
+            con.Open();
+
+            SqlCommand themeSwitchDatabase = new
+                SqlCommand("SELECT ThemeSwitch from Settings", con);
+            SqlDataReader reader = themeSwitchDatabase.ExecuteReader();
+            reader.Read();
+
+
+
+            // if themeSwitch sends True. That means dark theme was stored. So we must apply dark on startup.
+            if (reader.GetBoolean(0))
+            {
+                ResourceDictionary darkTheme = new ResourceDictionary
+                {
+                    Source = new Uri("ResourceDictionaries/DarkTheme.xaml", UriKind.Relative)
+                };
+
+                // apply dark theme
+                Application.Current.Resources.MergedDictionaries.Add(darkTheme);
+            } 
+            else // otherwise apply light theme if the answer if 0. In other words, if the last time switch state was stored as 0
+            {
+                ResourceDictionary lightTheme = new ResourceDictionary
+                {
+                    Source = new Uri("ResourceDictionaries/LightTheme.xaml", UriKind.Relative)
+                };
+
+                // apply dark theme
+                Application.Current.Resources.MergedDictionaries.Add(lightTheme);
+            }
+        }
     }
 }
